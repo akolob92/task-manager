7eb99c378aa598c1b946ecce3f5d501e902e8eee
akolob92 <akolob92@gmail.com>
2018-05-01 16:04:02 +0300

    fix: Update for mobile support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import {AppModule} from './app/app.module';
+import {environment} from './environments/environment';
+declare var require: any;
+
+const iosDragDropShim = require('../node_modules/drag-drop-webkit-mobile');
+const options = {enableEnterLeave: true, holdToDrag: 300}; // ios dragndrop
+iosDragDropShim(options);
 
 if (environment.production) {
   enableProdMode();
